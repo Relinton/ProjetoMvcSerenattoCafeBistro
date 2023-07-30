61026fc808458ff85ff49b61a99082cca7c5942a
Relinton Pinheiro <relinton.pinheiro.infouniron@gmail.com>
2023-07-30 18:52:01 -0400

    Projeto serenatto café, foi feito durante o curso de formação front-end utilizando o framework css bootstrap na plataforma Alura Cursos online

---
+++ wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿const inputCheck = document.querySelector('#modo-noturno')
+const elemento = document.querySelector('body')
+
+inputCheck.addEventListener('click', () => {
+    const modo = inputCheck.checked ? 'dark' : 'light'
+    elemento.setAttribute("data-bs-theme", modo)
+})
